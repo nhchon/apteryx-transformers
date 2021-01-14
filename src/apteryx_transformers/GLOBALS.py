394056703f666852da6391f5b1ce9845a0779c90
apteryxlabs <matthew@apteryxlabs.com>
2021-01-14 11:39:45 -0800

    globals and colorize formatting

--- src/apteryx_transformers/GLOBALS.py
+++ src/apteryx_transformers/GLOBALS.py
@@ -1,1 +1,10 @@
-MESSAGE='Hello, world!'
+TC_CLASS_MAP = {
+    0: '2800 - Semiconductors/Memory, Circuits/Measuring and Testing, Optics/Photocopying, Printing/Measuring and Testing',
+    1: '1600 - Biotechnology and Organic Chemistry',
+    2: '2600 - Communications',
+    3: '3700 - Mechanical Engineering, Manufacturing, Gaming, and Medical Devices/Processes',
+    4: '1700 - Chemical and Materials Engineering',
+    5: '3600 - Transportation, Construction, Electronic Commerce, Agriculture, National Security and License and Review',
+    6: '2100 - Computer Architecture and Software',
+    7: '2400 - Networking, Multiplexing, Cable, and Security'
+}
