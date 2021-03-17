7b76437a5ef4889e34769a0e25fdc145ef49bf9a
apteryxlabs <matthew@apteryxlabs.com>
2021-03-17 13:55:11 -0400

    annealing tools

---
+++ src/apteryx_transformers/simulated_annealing/heuristics/semantics.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch
+from torch.nn.functional import softmax
+from tqdm import tqdm
+
+from transformers import GPT2LMHeadModel, GPT2Config, GPT2TokenizerFast
+from sentence_transformers import SentenceTransformer
+from scipy.spatial.distance import cosine
+
+import logging
+
+
+class SemanticScorer:
+    def __init__(self, model_name='stsb-roberta-large', device='cpu'):
+        logger = logging.Logger('Semantic Scorer')
+
+        self.model = SentenceTransformer('stsb-roberta-large').to(device)
+
+    def __call__(self, sequence, target):
+        return self.similarity(sequence, target)
+
+    def similarity(self, sequence, target):
+        s_emb, t_emb = self.model.encode([sequence, target])
+        return (1 / (cosine(s_emb, t_emb)) / np.pi)
