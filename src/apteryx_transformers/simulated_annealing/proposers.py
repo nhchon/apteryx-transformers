24c0c72ed916a39e9c6ab356c738894b6e504daa
apteryxlabs <matthew@apteryxlabs.com>
2021-03-18 13:09:54 -0400

    proposer device

--- src/apteryx_transformers/simulated_annealing/proposers.py
+++ src/apteryx_transformers/simulated_annealing/proposers.py
@@ -8,11 +8,12 @@ from transformers import RobertaTokenizerFast, RobertaForMaskedLM
 
 
 class Proposer:
-    def __init__(self, tokenizer='roberta-base', model='roberta-base', spacy_model='en_core_web_sm'):
+    def __init__(self, tokenizer='roberta-base', model='roberta-base', spacy_model='en_core_web_sm', device='cpu'):
+        self.device = device
         self.logger = logging.Logger('Proposer')
         self.nlp = spacy.load(spacy_model)
         self.tokenizer = RobertaTokenizerFast.from_pretrained(tokenizer)
-        self.model = RobertaForMaskedLM.from_pretrained(model)
+        self.model = RobertaForMaskedLM.from_pretrained(model).to(self.device)
         self.ops = [('insert', self.insert), ('edit', self.edit), ('delete', self.delete)]
 
     def propose(self, s):
@@ -28,13 +29,13 @@ class Proposer:
 
         # Set probs of original vocab tokens to zero
         if not allow_same:
-            masked_ids = self.tokenizer(original_word, add_special_tokens=False).input_ids
+            masked_ids = self.tokenizer(original_word, add_special_tokens=False).to(self.device).input_ids
             probs[:, mask_idx, masked_ids] = 0
 
         edit = torch.multinomial(probs[:, mask_idx], num_samples=1, replacement=True).item()
         new[:, mask_idx] = edit
 
-        return self.tokenizer.batch_decode(new, skip_special_tokens=True)[0] #, mask_idx, original_word
+        return self.tokenizer.batch_decode(new, skip_special_tokens=True)[0]  # , mask_idx, original_word
 
     def insert(self, s):
         masked, inputs, mask_idx, original_word = self.mask(s, mode='insert')
@@ -44,7 +45,7 @@ class Proposer:
         edit = torch.multinomial(out.logits.softmax(2)[:, mask_idx], num_samples=1, replacement=True).item()
         new[:, mask_idx] = edit
 
-        return self.tokenizer.batch_decode(new, skip_special_tokens=True)[0] #, mask_idx, original_word
+        return self.tokenizer.batch_decode(new, skip_special_tokens=True)[0]  # , mask_idx, original_word
 
     def delete(self, s):
         '''
@@ -52,27 +53,25 @@ class Proposer:
         '''
         to_mask, start_idx, end_idx, pos, words = self.mask(s, mode='delete')
 
-
         left_char = s[start_idx - 1] if start_idx > 0 else ''
         right_char = s[end_idx] if end_idx < len(s) else ''
 
         n_whitespace_remaining = sum([left_char in string.whitespace, right_char in string.whitespace])
 
-
         if n_whitespace_remaining == 2:
             # Delete char to the right - it doesn't matter, both are whitespace
-            return s[:start_idx] + s[end_idx + 1:]#, to_mask
+            return s[:start_idx] + s[end_idx + 1:]  # , to_mask
 
         elif n_whitespace_remaining == 1:
             # Don't delete anything
-            return s[:start_idx] + s[end_idx:]#, to_mask
+            return s[:start_idx] + s[end_idx:]  # , to_mask
 
         elif n_whitespace_remaining == 0:
             # Replace missing whitespace
-            return s[:start_idx] + ' ' + s[end_idx:]#, to_mask
+            return s[:start_idx] + ' ' + s[end_idx:]  # , to_mask
         else:
             print('Should never reach here.')
-            return s[:start_idx] + s[end_idx:]#, to_mask
+            return s[:start_idx] + s[end_idx:]  # , to_mask
 
         # elif left_char in string.whitespace:
         #     #Right char is either punct or part of another word; remove in both directions
@@ -80,8 +79,6 @@ class Proposer:
         # elif right_char in string.whitespace:
         #     #Left char is likely punct; leave both
         #     return s[:start_idx] + s[end_idx:]
-
-
 
     def mask(self, s, mode='insert'):
         # encoded = self.tokenizer(s, return_tensors='pt')
@@ -116,7 +113,7 @@ class Proposer:
             self.logger.error(f'Mask mode "{mode}" not valid')
             assert False
 
-        inputs = self.tokenizer(masked, return_tensors="pt")
+        inputs = self.tokenizer(masked, return_tensors="pt").to(self.device)
 
         mask_idx = (inputs.input_ids[0] == self.tokenizer.mask_token_id).nonzero().item()
 
