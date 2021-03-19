9a81513206d6f4bd766c5e59af625affc1eb8407
apteryxlabs <matthew@apteryxlabs.com>
2021-03-19 13:33:20 -0400

    updates

--- src/apteryx_transformers/simulated_annealing/annealers.py
+++ src/apteryx_transformers/simulated_annealing/annealers.py
@@ -1,19 +1,25 @@
 import numpy as np
 from tqdm import tqdm
 
+
 class Annealer:
     def __init__(self, scorer, proposer):
+        # Score output is {'total': num, 'constituent_1': num, ...'constituent_N':num}
         self.scorer = scorer
+
         self.proposer = proposer
 
     def propose_until_accepted(self, y, T):
         its = 1
         while True:
-            candidate = self.proposer.propose(y)
-            p_accept = min(1, (np.exp((self.scorer.score(candidate['output']) - self.scorer.score(y)) / T)))
+            proposal = self.proposer.propose(y)
+            candidate_seq = proposal['output']
+            candidate_score = self.scorer.score(candidate_seq)
+            y_score = self.scorer.score(y)
+            p_accept = min(1, (np.exp((candidate_score['total'] - y_score['total']) / T)))
             accepted = (np.random.uniform(0, 1) < p_accept)
             if accepted:
-                return candidate['output'], its
+                return candidate_seq, its, candidate_score, y_score
             its += 1
 
     def anneal(self,
@@ -26,14 +32,20 @@ class Annealer:
         y_hist = [y]
         t_hist = [T_init]
         it_hist = [0]
+        candidate_score_hist = []
+        y_score_hist = []
         for t in tqdm(range(max_search)):
             T = max(T_init - (C * t), eps)
-            y, its = self.propose_until_accepted(y, T)
+            y, its, candidate_score, y_score = self.propose_until_accepted(y, T)
             y_hist.append(y)
             t_hist.append(T)
             it_hist.append(its)
+            candidate_score_hist.append(candidate_score)
+            y_score_hist.append(y_score)
 
         return {'y': y,
                 'y_hist': y_hist,
                 't_hist': t_hist,
-                'it_hist': it_hist}
+                'it_hist': it_hist,
+                'y_score_hist': y_score_hist,
+                'candidate_score_hist': candidate_score_hist}
